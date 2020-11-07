24fd21370c4851e6cf4c4086d1e900da9585cff6
Anton Rotov <anton_rotov@epam.com>
2020-11-07 17:45:27 +0300

    added bootstrap

--- src/main/java/com/trading/report/controller/ReportController.java
+++ src/main/java/com/trading/report/controller/ReportController.java
@@ -5,19 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.tinkoff.invest.openapi.models.Currency;
-import ru.tinkoff.invest.openapi.models.operations.OperationsList;
 
 @Controller
 public class ReportController {
-    @Autowired
-    private OperationService service;
+  @Autowired
+  private OperationService operationService;
 
-    @GetMapping("/report")
-    public String greeting(Model model) {
-        model.addAttribute("usd", service.getOperations().get(Currency.USD));
-        model.addAttribute("eur", service.getOperations().get(Currency.EUR));
-        model.addAttribute("rub", service.getOperations().get(Currency.RUB));
-        return "report";
-    }
+  @GetMapping("/report")
+  public String getReport(@RequestParam(value = "currency", required = false) String currency, Model model) {
+    Currency currentCurrency = currency == null ? Currency.USD : Currency.valueOf(currency.toUpperCase());
+    model.addAttribute("report", operationService.getOperations().get(currentCurrency));
+    model.addAttribute("currency", currentCurrency.toString());
+    return "report";
+  }
 }
--- src/main/java/com/trading/report/service/DictionaryService.java
+++ src/main/java/com/trading/report/service/DictionaryService.java
@@ -7,23 +7,35 @@ import ru.tinkoff.invest.openapi.models.market.Instrument;
 import ru.tinkoff.invest.openapi.models.market.InstrumentsList;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 @Service
 public class DictionaryService {
-    @Autowired
-    private OpenApi api;
+  @Autowired
+  private OpenApi api;
 
-    private Map<String, Instrument> instruments;
+  private Map<String, Instrument> instruments;
 
-    @PostConstruct
-    private void init() {
-        InstrumentsList instrumentsList = api.getMarketContext().getMarketStocks().join();
-        instruments = instrumentsList.instruments.stream().collect(Collectors.toMap(i -> i.figi, i -> i));
-    }
+  @PostConstruct
+  private void init() {
 
-    public Instrument getInstrumentByFigi(String figi) {
-        return instruments.get(figi);
-    }
+    List<CompletableFuture<InstrumentsList>> futureList = new ArrayList<>();
+    futureList.add(api.getMarketContext().getMarketStocks());
+    futureList.add(api.getMarketContext().getMarketCurrencies());
+    futureList.add(api.getMarketContext().getMarketBonds());
+    futureList.add(api.getMarketContext().getMarketEtfs());
+
+    instruments = futureList.stream()
+            .flatMap(item -> item.join().instruments.stream())
+            .collect(Collectors.toMap(i -> i.figi, i -> i));
+
+  }
+
+  public Instrument getInstrumentByFigi(String figi) {
+    return instruments.get(figi);
+  }
 }
